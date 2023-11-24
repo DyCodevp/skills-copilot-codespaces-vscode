7d5b703454c84dbdb4865c0199d547598317f281
Dylan chaves <Dylanpotoy@gmail.com>
2023-11-24 20:04:30 +0000

    copilo 3 commit

---
+++ comments.js
@@ -0,0 +1,12 @@
+// Create web server 
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello, World!\n');
+});
+
+server.listen(3000, '127.0.0.1', () => {
+  console.log('Server is running at http://127.0.0.1:3000/');
+});
